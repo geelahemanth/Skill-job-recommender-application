cbd38b70414d04db119d14de38f443b84b51b439
geelahemanth <109453839+geelahemanth@users.noreply.github.com>
2024-01-18 10:02:44 +0530

    Add files via upload

---
+++ Assignments/P Nandhunaidu/Assignment 2/app.py
@@ -0,0 +1,19 @@
+import flask from Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
